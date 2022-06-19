777291ee6b94637c4262a3d55c7d2864a2f1df21
valenciomuskiet <94371531+valenciomuskiet@users.noreply.github.com>
2022-06-19 16:36:13 +0200

    Update Program.cs
    Test 2

--- githubprojectdepot/ProjectDepot/ProjectDepot/Program.cs
+++ githubprojectdepot/ProjectDepot/ProjectDepot/Program.cs
@@ -7,6 +7,8 @@ namespace ProjectDepot
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Test 2");
+ 
         }
     }
 }
